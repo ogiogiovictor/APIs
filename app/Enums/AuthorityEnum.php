0ef410b0f96ab0c64b96317e04d954488373e7dd
OgiogioVictor <ogiogiovictor@gmail.com>
2023-04-15 06:28:30 -0600

    added authority

---
+++ app/Enums/AuthorityEnum.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Enums;
+
+enum AuthorityEnum: string
+{
+    case HEADQUATERS = 'hq';
+    case REGION = 'region';
+    case BUSINESSHUB = 'business_hub';
+    case SERVICECENTER = 'service_center';
+
+    public function label(): string
+    {
+        return match($this) {
+            self::ADMIN => 'hq',
+            self::REGION => 'region',
+            self::BUSINESSHUB => 'business_hub',
+            self::SERVICECENTER => 'service_center',
+        };
+    }
+}
+
+
+// use Spatie\Enum\Laravel\Enum;
+
+// final class AuthorityEnum extends Enum
+// {
+// }
--- database/migrations/2023_04_15_121022_add_role_to_users_table.php
+++ database/migrations/2023_04_15_121022_add_role_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('authority')->after('password')->default('user');
+            $table->string('authority')->after('password')->default('business_hub');
         });
     }
 
