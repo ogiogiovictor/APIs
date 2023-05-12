60b4218099da6ec1940c3827bb60132d5fc97ef2
ogiogiovictor <ogiogiovictor@gmail.com>
2023-05-12 16:23:28 +0100

    vipush

--- app/Repositories/AssetRepository.php
+++ app/Repositories/AssetRepository.php
@@ -88,6 +88,10 @@ class AssetRepository implements AssetRepositoryInterface
 
    public function getAllThirty() {
 
+    $elevenDt = DTWarehouse::where('assettype', AssetEnum::DT_eleven()->value)->count();
+    $thirtyDt = DTWarehouse::where('assettype', AssetEnum::DT_thirty_three()->value)->count();
+    $dtTotal = DTWarehouse::count();
+
     $getDTs = DTWarehouse::withCount('getCustomerCount')
             ->where('assettype', AssetEnum::DT_thirty_three()->value)
             ->paginate(20);
