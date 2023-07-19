359c84792ff036bf4aee17f60558ddc0a2243859
ogiogiovictor <ogiogiovictor@gmail.com>
2023-07-19 17:00:45 +0100

    added new ones

--- app/Http/Controllers/CAAD/CaadController.php
+++ app/Http/Controllers/CAAD/CaadController.php
@@ -12,6 +12,7 @@ use App\Models\FileCAAD;
 use App\Models\ProcessCAAD;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Validator;
 
 class CaadController extends BaseApiController
 {
@@ -24,41 +25,81 @@ class CaadController extends BaseApiController
     }
 
 
-    public function addCAAD(Request $request){
+    public function addCAAD(CaadRequest $request){
 
-      // $file =  $request->file('file_upload');
+        $validator = Validator::make($request->all(), [
+            'file_upload' => 'required|array',
+            'file_upload.*' => 'file|mimes:jpeg,jpg,png,pdf|max:2048', // Add allowed file types here
+            // other validation rules for other form fields if required
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError("Validation Error", $validator->errors(), Response::HTTP_BAD_REQUEST);
+        }
+    
+
         try {
 
-           
+            $processCAAD = ProcessCAAD::create([
+                'accountNo' => $request->accountNo,
+                'phoneNo' => $request->phoneNo,
+                'surname' => $request->surname,
+                'lastname' => $request->lastname,
+                'othername' => $request->othername,
+                'service_center' => $request->service_center,
+                'meterno' => $request->meterno,
+                'accountType' => $request->accountType,
+                'transtype' => $request->transtype,
+                'meter_reading' => $request->meter_reading,
+                'transaction_type' => $request->transaction_type,
+                'effective_date' => $request->effective_date,
+                'amount' => $request->amount,
+                'remarks' => $request->remarks,
+                'file_upload_id' => 0,
 
+            ]);
+
+           
+              // Check if the destination folder exists and has write permissions is_writable
+              $destinationPath = public_path('customercaad/');
+              if (!file_exists($destinationPath)) {
+                  return $this->sendError("Error", "Destination folder 'customercaad' is does not exist.", Response::HTTP_INTERNAL_SERVER_ERROR);
+              }else if(!is_writable($destinationPath)){
+                  return $this->sendError("Error", "Destination folder 'customercaad' is not writable.", Response::HTTP_INTERNAL_SERVER_ERROR);
+              }
+
+             
              //Handle file upload
             if ($request->has('file_upload')) {
-                $file = $request->file('file_upload');
-                $fileName = $file->getClientOriginalName();
 
-                 // Check if the destination folder exists and has write permissions is_writable
-            $destinationPath = public_path('customercaad/');
-
-            //return $destinationPath;
-
-            if (!file_exists($destinationPath)) {
-                return $this->sendError("Error", "Destination folder 'customercaad' is does not exist.", Response::HTTP_INTERNAL_SERVER_ERROR);
-            }else if(!is_writable($destinationPath)){
-                return $this->sendError("Error", "Destination folder 'customercaad' is not writable.", Response::HTTP_INTERNAL_SERVER_ERROR);
-            }
-
-
-            $file->storeAs('customercaad', $fileName, 'public');
-
+                $files = $request->file('file_upload');
                
-            
-            }else {
-            return $this->sendError("Error", "File not found in the request.", Response::HTTP_BAD_REQUEST);
-        }
+                foreach ($files as $file) {
+
+                    $timestamp = now()->timestamp; // Generate the current Unix timestamp
+                    $date = now()->format('Ymd'); // Format the current date as YYYYMMDD  
+                    // Combine the timestamp, date, and extension to form the unique filename
+                    $uniqueFileName = $date . '_' . $timestamp;
+
+                    $fileName = $uniqueFileName.''. uniqid() . '_' . $file->getClientOriginalName();
+                    $extension = $file->getClientOriginalExtension();
+
+                    $file->storeAs('customercaad', $fileName, 'public');
+
+                    $uploadfile = FileCAAD::create([
+                        'process_caad_id' => $processCAAD->id,
+                        'file_name' => $fileName,
+                        'file_size' => $file->getSize(),
+                        'file_type' => $file->getClientMimeType(),
+                        'file_link' => 'customercaad/',
+                    ]);
+                }
+    
+            }
 
          
 
-          return $this->sendSuccess("Completed", "Get All Results", Response::HTTP_CREATED);
+          return $this->sendSuccess($processCAAD, "File Successfully Uploaded", Response::HTTP_CREATED);
 
         }catch(\Exception $e){
 
